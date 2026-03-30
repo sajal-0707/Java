eb3646cf7ec5a6ff42946511dda52c8f5c30ac5d
Sajal Chourasia <sajal007@rev-178-16-16-140.deac.net>
2026-03-30 23:16:47 +0530

    Addition of two numbers, values taken from users

---
+++ addition.java
@@ -0,0 +1,13 @@
+public class addition {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the two numbers and store the result in a new variable 'sum'
+        int sum = num1 + num2;
+
+        // Print the result to the console
+        System.out.println("The sum is: " + sum);
+    }
+}
