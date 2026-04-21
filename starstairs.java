3639bc321a2e949a122d8441ffc62a2005beb511
Sajal Chourasia <sajal007@adsl178.16.0.13.manx.net>
2026-04-21 23:13:13 +0530

    Stairs pattern made from stars

---
+++ starstairs.java
@@ -0,0 +1,14 @@
+public class starstairs {
+    public static void main(String[]srgs)
+    {
+        int i,j,n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
