47853d7d8ee75f05dfc45a0969f0a44fcd88b64c
Sajal Chourasia <sajal007@adsl178.16.0.13.manx.net>
2026-04-22 22:59:01 +0530

    Can vote or not

---
+++ voters.java
@@ -0,0 +1,13 @@
+public class voters {
+    public static void main(String[]args)
+    {
+        float age=20;
+        if(age>=18)
+        {
+            System.out.println("Can vote");
+        }
+        else {
+            System.out.println("Can not vote");
+        }
+    }
+}
