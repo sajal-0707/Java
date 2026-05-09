96d5c4dbbe44c28e68e5c36c6f4f9f835c5688df
Sajal Chourasia <sajal007@rev-178-16-18-40.deac.net>
2026-05-09 21:48:26 +0530

    Rolling a dice in java

---
+++ diceroll.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class diceroll {
+    public static void main(String[] args)
+    {
+        Random random= new Random();
+        int num= random.nextInt(1,7);
+        System.out.println(num);
+    }
+}
