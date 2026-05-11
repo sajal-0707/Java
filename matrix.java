95075f5a8f1c885b0569c7b5a05ad88518389e78
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-05-11 10:11:43 +0530

    To make a matrix of a symbol

--- matrix.java
+++ matrix.java
@@ -4,11 +4,11 @@ public class matrix
     public static void main(String[] args)
     {
         Scanner scanner= new Scanner(System.in);
-        System.out.print("Enter number of columns= ");
+        System.out.print("Enter number of columns = ");
         int columns= scanner.nextInt();
-        System.out.print("Enter number of rows= ");
+        System.out.print("Enter number of rows = ");
         int rows= scanner.nextInt();
-        System.out.print("Enter the symbol tou want to fill in the matrix= ");
+        System.out.print("Enter the symbol tou want to fill in the matrix = ");
         char symbol= scanner.next().charAt(0);
         for(int i=1;i<=rows;i++)
         {
