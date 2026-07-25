b5aa25911ca2f8ff0197ce7dc75d5f8521409d67
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-25 23:32:04 +0530

    Adding two numbers

--- addion.java
+++ addion.java
@@ -4,10 +4,10 @@ public class addion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter first number: ");
+        System.out.println("Enter first number : ");
         int num1 = sc.nextInt();
 
-        System.out.println("Enter second number: ");
+        System.out.println("Enter second number : ");
         int num2 = sc.nextInt();
 
         int sum = num1 + num2;
