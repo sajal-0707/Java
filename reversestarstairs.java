db30397d0ab87afd500c528e110e14ee66344f6d
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-26 23:45:36 +0530

    Reverse stairs

--- reversestarstairs.java
+++ reversestarstairs.java
@@ -2,7 +2,7 @@ public class reversestarstairs {
     public static void main(String[]srgs)
     {
         int i,j,n=5;
-        for(i=n;i>=1;i--)
+        for(i=n;i>=1;i-- )
         {
             for(j=1;j<=i;j++)
             {
