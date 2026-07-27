dd2929a42c209b9eef8301f46a53188f511a20d0
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-27 23:03:14 +0530

    Armstrong Number

---
+++ armstrong.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class armstrong
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner= new Scanner(System.in);
+        int n=0 ;
+        int tot=0;
+        System.out.print("Enter a number = ");
+        int a = scanner.nextInt();
+        int ori = a;
+        int temp=ori;
+        for(int i=ori;i>0;i/=10)
+        {
+            n++;
+        }
+        for (int i=n;i>0;i--)
+        {
+            int last=temp%10;
+            tot+=(int)Math.pow(last,n);
+            temp/=10;
+        }
+        System.out.println(tot);
+        if (tot == ori)
+        {
+            System.out.println(ori + " is an Armstrong number");
+        }
+        else
+        {
+                System.out.println(ori + " is not an Armstrong number");
+        }
+        scanner.close();
+    }
+}
